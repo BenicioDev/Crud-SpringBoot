6e6bb0fb2ae7240cbe56d525615f160530486d13
Gabriel Benicio <gabrielvitor083@gmail.com>
2023-10-13 18:50:38 -0300

    implementação de dados JSON e criação da classe Record

---
+++ projetocrud/src/main/java/com/projeto/projetocrud/ServidorPublicoApplicationRunner.java
@@ -0,0 +1,13 @@
+package com.projeto.projetocrud;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class ServidorPublicoApplicationRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
