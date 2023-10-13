fe4215c53c0b99d02cdc6da19835d5e0d8cc594f
Gabriel Benicio <gabrielvitor083@gmail.com>
2023-10-13 20:16:23 -0300

    Implementação dos métodos de busca para poder realizar teste no projeto.

--- projetocrud/src/main/java/com/projeto/projetocrud/ServidorPublicoApplicationRunner.java
+++ projetocrud/src/main/java/com/projeto/projetocrud/ServidorPublicoApplicationRunner.java
@@ -1,8 +1,14 @@
 package com.projeto.projetocrud;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.swing.JOptionPane;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
+import com.projeto.projetocrud.model.ServidorPublico;
 import com.projeto.projetocrud.service.ServidorPublicoService;
 
 public class ServidorPublicoApplicationRunner implements CommandLineRunner {
@@ -13,6 +19,42 @@ public class ServidorPublicoApplicationRunner implements CommandLineRunner {
 	@Autowired
 	public void setServidorPublicoService (ServidorPublicoService servidorService) {
 		this.servidorService = servidorService;
+	}
+	
+	//Método 1 - Chamar Service - listAll()
+	public void listAll() {
+		List<ServidorPublico> servidorespublicos = servidorService.listAll();
+		
+		if (servidorespublicos.size() !=0) {
+			servidorespublicos.forEach(
+					servidor -> {
+						System.out.println("»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»");
+						System.out.println(servidor.matricula());
+						System.out.println(servidor.nome());
+						System.out.println(servidor.foto());
+						System.out.println(servidor.orgao());
+					}
+					);
+		}
+		else {
+			System.out.print("Arquivo vazio");
+		}
+	}
+	
+	//Método 2 - receber matrícula
+	public void listByMatricula() {
+		long matricula = Long.parseLong(JOptionPane.showInputDialog("Digite matrícula desejada: "));
+		Optional<ServidorPublico> servidorEncontrado = servidorService.listByMatricula(matricula);
+		
+		if(servidorEncontrado.isPresent()) {
+			System.out.println("»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»");
+			System.out.println(servidorEncontrado.get().matricula());
+			System.out.println(servidorEncontrado.get().nome());
+			System.out.println(servidorEncontrado.get().foto());
+			System.out.println(servidorEncontrado.get().orgao());
+		} else {
+			System.out.print("Matrícula inexistente");
+		}
 	}
 	
 	
